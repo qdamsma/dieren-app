f09c13b662e3047d6ba7b40da13b09a881ca7d48
qdamsma <qdamsma@gmail.com>
2024-10-18 15:15:20 +0200

    test

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contacts', function () {
+    return"<h1>All contacts</h1>";
+})->name('contacts.index');
+
+Route::get('/contacts/create', function () {
+    return"<h1>Add new contacts</h1>";
+})->name('contacts.create');
+
+Route::get('/contacts/{id}', function ($id) {
+    return "Contact ".$id;
+})->name('contacts.show');;
+ 
